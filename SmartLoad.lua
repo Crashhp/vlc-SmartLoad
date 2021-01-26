d2843da5f0b5e1ab326e5049cf1a4b14196389c4
Мамаев К.А <mamaevka@umssoft.com>
2021-01-26 11:35:35 +0700

    Initial fix for path and encoding

--- SmartLoad.lua
+++ SmartLoad.lua
@@ -9,88 +9,38 @@ function descriptor()
 end
 
 function activate()
-    -- vlc.playlist.enqueue({{path = ""}})
-    -- if (true) then return end
-
     local playlistItems = vlc.playlist.get("normal", false).children
-    -- vlc.msg.dbg(vlc.playlist.current())
-    if (#playlistItems == 0) then 
-        vlc.msg.info("[SmartLoad] No items in playlist!")
-        vlc.deactivate()
-        return 
-    end
+    local currentItem = playlistItems[#playlistItems]
+    local fileExtension = GetFileExtension(currentItem.name)
+    local folderPath = string.gsub(string.match(currentItem.path, ".*[\\\\/]"), "file:///", "")
+    local folderPath = string.gsub(folderPath, "%%20", " ")
+    local folderPath = string.gsub(folderPath, "%%5B", "[")
+    local folderPath = string.gsub(folderPath, "%%5D", "]")
 
-    local curItem = playlistItems[#playlistItems] -- playlist.current_item()
-    for k,v in pairs(curItem) do vlc.msg.dbg(tostring(k) .. " " .. tostring(v)) end
-    local ignoredPaths = { "."; ".." }
-
-    for key,item in ipairs(playlistItems) do
-        table.insert(ignoredPaths, getFilename(item.path))
-    end
-    
-    if (not (string.sub(curItem.path, 1, 7) == "file://")) then
-        vlc.msg.info("[SmartLoad] Last item is not a proper file!")
-        vlc.deactivate()
-        return 
-    end
-
-    local folderPath = getFolder(curItem.path)
-    local curItemName = getFilename(curItem.path)
-    -- for k,v in pairs(vlc.net.opendir(folderPath)) do vlc.msg.dbg(tostring(k) .. " " .. tostring(v)) end
+    --vlc.msg.info(currentItem.path) 
+    --vlc.msg.info(folderPath)
 
     local files = vlc.io.readdir(folderPath)
+
     table.sort(files)
 
-    local beforeFile = true
-
     for _, item in ipairs(files) do
-        if ((curItemName >= item) or (arrayContains(item, ignoredPaths))) then
-            vlc.msg.dbg("Skip: " .. item)
-        else
-            vlc.msg.dbg("Trying to add: " .. item)
-            vlc.playlist.enqueue({{path = "file://" .. folderPath .. item; name = item}})
+        if (EndsWith(item, fileExtension)) then
+            if (currentItem.name < item) then
+                vlc.playlist.enqueue({{path = "file:///" .. folderPath .. item; name = item}})
+            end
         end
     end
+
     vlc.deactivate()
 end
 
-function arrayContains(value, arr)
-    for _, item in ipairs(arr) do
-        if (item == value) then
-            return true
-        end
-    end
-
-    return false
+function GetFileExtension(url)
+    return url:match("^.+(%..+)$")
 end
 
-function getFolder(path)
-    local folderPath = string.match(path, ".*[\\\\/]")
-    folderPath = string.gsub(folderPath, "file://", "")
-    -- vlc.msg.dbg(folderPath)
-    return unpercent(folderPath)
-end
-
-function getFilename(path)
-    local filename = string.match(path, "[^\\\\/]*$")
-    return unpercent(filename)
-end
-
-function unpercent(str)
-
-    return vlc.strings.url_parse(str)
-    -- local matchCount
-    -- local parsedString
-    -- local percMatch = "%%%x%x"
-    -- function parsePercent(str)
-    --    local varCode = tonumber(string.sub(str, 2), 16)
-    --     -- vlc.msg.dbg("matched " .. str .. " charCode " .. varCode .. " char: " .. string.char(varCode))
-    --     return string.char(varCode) -- should be utf8.char(varCode)
-    -- end
-    
-    -- parsedString, matchCount = string.gsub(str, percMatch, parsePercent)
-    -- -- vlc.msg.dbg(matchCount .. " " .. parsedString)
-    -- return parsedString
+function EndsWith(str, ending)
+    return ending == "" or str:sub(-#ending) == ending
 end
 
 function deactivate()
